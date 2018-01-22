94262d2a969cec1f677ab70a23a36fe9d5bbdac6
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 05:15:23 +0300

    Исправлены ошибки

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,32 +1,25 @@
 import sys
+from collections import Counter
+
 
 def load_data(filepath):
-    words = dict()
+    words = []
     with open(filepath, "r") as file:
         for line in file:
-            lst = [word for word in line.split()]
-            for word in lst:
-                word = word.lower()
-                if word not in words:
-                    words[word] = 1
-                else:
-                    words[word] += 1
+            words.extend(line.split())
     return words
 
 
 def get_most_frequent_words(words):
-    count = 0
-    for i in sorted(words.items(), key=lambda x:(-x[1],x[0])):
-        if (count < 10):
-            print(count + 1, i[0], ':= ', i[1])
-            count += 1
-        else:
-            break
+    counter = Counter(words)
+    pairs = [(-pair[1], pair[0]) for pair in counter.most_common(10)]
+    for i in sorted(pairs):
+        print(i[0] * -1, i[1])
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print ("Error: define file path")
-        print ("Usage example: python lang_frequency.py <path to file>")
+        print("Error: define file path")
+        print("Usage example: python lang_frequency.py <path to file>")
     else:
-         words = load_data(sys.argv[1])
-         get_most_frequent_words(words)
+        words = load_data(sys.argv[1])
+        get_most_frequent_words(words)
