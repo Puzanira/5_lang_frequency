ff576e349ea25b09b8862fe6c33c9055d42ef3b3
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 23:00:49 +0300

    Removes pairs

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,36 +1,31 @@
 import sys
-from collections import Counter
 import re
+from collections import Counter
 
 
 def load_data(filepath):
-    file = open(filepath, 'r')
-    try:
-        text = file.read()
-    finally:
-        file.close()
-    all_words_list = re.findall(r"[\w']+", text)
-    return all_words_list
+    with open(filepath, 'r') as file_handler:
+        read_data = file_handler.read()
+    return read_data
 
 
-def get_most_frequent_words(words):
-    words_counter = Counter(words)
+def get_most_frequent_words(text):
+    words_list = re.findall(r"[\w']+", text)
+    words_counter = Counter(words_list)
     max_words = 10
-    common_words = words_counter.most_common(max_words)
-    word_pairs = [(pair[1], pair[0]) for pair in common_words]
-    return word_pairs
+    return words_counter.most_common(max_words)
 
 
 def print_words_list(word_dict):
-    for count, word in word_dict:
+    for word, count in word_dict:
         print(count, word)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Error: define file path")
-        print("Usage example: python lang_frequency.py <path to file>")
+        print('Error: define file path')
+        print('Usage example: python lang_frequency.py <path to file>')
     else:
-        words_list = load_data(sys.argv[1])
-        sorted_list = get_most_frequent_words(words_list)
+        text = load_data(sys.argv[1])
+        sorted_list = get_most_frequent_words(text)
         print_words_list(sorted_list)
