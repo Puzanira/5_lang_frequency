858dd817c841c9f93fe939195c7221fa20d51e3d
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 03:08:06 +0300

    Доделан частотный словарь

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,32 @@
-
+import sys
 
 def load_data(filepath):
-    pass
+    words = dict()
+    with open(filepath, "r") as file:
+        for line in file:
+            lst = [word for word in line.split()]
+            for word in lst:
+                word = word.lower()
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] += 1
+    return words
 
 
-def get_most_frequent_words(text):
-    pass
-
+def get_most_frequent_words(words):
+    count = 0
+    for i in sorted(words.items(), key=lambda x:(-x[1],x[0])):
+        if (count < 10):
+            print(count + 1, i[0], ':= ', i[1])
+            count += 1
+        else:
+            break
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) != 2:
+        print ("Error: define file path")
+        print ("Usage example: python lang_frequency.py <path to file>")
+    else:
+         words = load_data(sys.argv[1])
+         get_most_frequent_words(words)
